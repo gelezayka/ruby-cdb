1b9c30b1f88f20f22fccf96f1b63800230f2b325
Charlie Root <root@srv2.getapic.me>
2017-05-14 19:10:23 +0300

    fix use clang

--- extconf.rb
+++ extconf.rb
@@ -1,5 +1,7 @@
 require 'mkmf'
 
+CONFIG['CC'] = 'clang'
+CONFIG['CXX'] = 'clang'
 $LOCAL_LIBS += ['cdb.a', 'alloc.a', 'error.o', 'seek_set.o', 'byte_copy.o',
                 'byte_diff.o', 'buffer_get.o', 'buffer_put.o', 'str_len.o',
                 'byte_cr.o', 'buffer.o', 'uint32_pack.o', 'uint32_unpack.o'].
